5ea1aa9c8f822f7bf204a285c06d7ce1c65a41e4
Elliot Su <sodinfeliz@gmail.com>
2024-04-09 09:39:07 +0800

    [DOCS] Update the examples

--- examples/example.py
+++ examples/example.py
@@ -1,19 +1,30 @@
-import sys
 import os
+import sys
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
-from sqlalchemy.exc import DataError
-from examples.example_table import Project, db
-
+from examples.example_table import TestTable, db
 
 if __name__ == "__main__":
-    try:
-        # Get all projects with project_name='adsadsy'
-        with db.session() as session:
-            sql_results = session.query(Project).filter_by(project_name='adsadsy').all()
-            for sql_result in sql_results:
-                print(sql_result.project_status)
-    except DataError as e:
-        print(e)
+    # Create the records
+    db.create(TestTable, project_name="Project 1", annotation="This is project 1")
+    db.create(TestTable, project_name="Project 2", annotation="This is project 2")
+    db.create(TestTable, project_name="Project 2", annotation="This is project 2")
+
+    # Update the records
+    db.update(TestTable, filters={"project_name": "Project 2"}, annotation="This is project 2 updated")
+
+    # Retrieve the records
+    records = db.retrieve(TestTable, project_name="Project 2")
+    for record in records:
+        print(record.annotation)
+
+    # Delete the records
+    db.delete(TestTable, project_name="Project 1")
+
+    # Check if the record exists
+    exists =  db.exists(TestTable, project_name="Project 2")
+    print(exists)
+
+
--- examples/example_table.py
+++ examples/example_table.py
@@ -1,13 +1,15 @@
 import uuid
+
 from sqlalchemy import Column, Text
-from sqlalchemy.dialects.postgresql import UUID, JSON, BIGINT
-from pyalchemyadmin import PostgreDBManager, DBManager, SQLiteDBManager
+from sqlalchemy.dialects.postgresql import UUID
+
+from pyalchemyadmin import PostgreDBManager
 
 DATABASE = 'postgres'
-USER = 'admin'
-PASSWORD = 'admin'
+USER = 'postgres'
+PASSWORD = 'password'
 HOST = '127.0.0.1'
-PORT = 7776
+PORT = 5432
 
 
 db = PostgreDBManager(
@@ -20,17 +22,12 @@ db = PostgreDBManager(
 )
 
 
-class Project(db.base):
-    __tablename__ = 'project'
+class TestTable(db.base):
+    __tablename__ = 'test_table'
     
     project_uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     project_name = Column(Text, nullable=False)
-    project_status = Column(JSON)
-    project_path = Column(Text)
     annotation = Column(Text)
-    cover_image_name = Column(Text)
-    cover_image_type = Column(Text)
-    create_time = Column(BIGINT)
 
 
-db.create_all()
+db.create_all_tables()
